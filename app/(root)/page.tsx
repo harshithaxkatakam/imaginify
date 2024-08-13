92683d310e2d40d0ff44ec0fb84d872c57778aa6
harshithaxkatakam <harshithaxkatakam@gmail.com>
2024-08-12 23:47:29 -0500

    clerk-authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      Home
+      <p>
+        Home
+      </p>
     </div>
   );
 }
