04265c71c428518421e0099d0d8ac0ccf82dc2ac
harshithaxkatakam <harshithaxkatakam@gmail.com>
2024-08-16 19:55:29 -0500

    image-server-actions

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -22,6 +22,9 @@ import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import MediaUploader from "./MediaUploader";
 import TransformedImage from "./TransformedImage";
 import { updateCredits } from "@/lib/actions/user.actions"
+import { getCldImageUrl } from "next-cloudinary";
+import { addImage, updateImage } from "@/lib/actions/image.actions";
+import { useRouter } from "next/navigation";
 
 
 export const formSchema = z.object({
@@ -42,7 +45,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
   const [isTransforming, setIsTransforming] = useState(false);
   const [isPending, startTransition] = useTransition()
   const [transformationConfig, setTransformationConfig] = useState(config)
-
+  const router = useRouter()
 
   const onSelectFieldHandler = (value: string, onChangeField: (value: string) => void) => {
     const imageSize = aspectRatioOptions[value as AspectRatioKey];
@@ -52,6 +55,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
       width: imageSize.width,
       height: imageSize.height
     }));
+    setNewTransformation(transformationType.config)
   }
 
   const onInputChangeHandler = (fieldName: string, value: string, type: string, onChangeField: (value: string) => void) => {
@@ -91,8 +95,64 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values)
+    setIsSubmitting(true)
+    if (data || image) {
+      const transformationUrl = getCldImageUrl({
+        width: image?.width,
+        height: image?.height,
+        src: image?.publicId,
+        ...transformationConfig
+      })
+      const imageData = {
+        title: values.title,
+        publicId: image?.publicId,
+        transformationType: type,
+        width: image?.width,
+        height: image?.height,
+        config: transformationConfig,
+        secureURL: image?.secureURL,
+        transformationURL: transformationUrl,
+        aspectRatio: values.aspectRatio,
+        prompt: values.prompt,
+        color: values.color
+      }
+      if (action === 'Add') {
+        try {
+          const newImage = await addImage({
+            image: imageData,
+            userId: userId,
+            path: '/'
+          })
+          if (newImage) {
+            form.reset()
+            setImage(data)
+            router.push(`/transformations/${newImage._id}`)
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      if(action === 'Update'){
+        try {
+          const updatedImage = await updateImage({
+            image: {
+              ...imageData,
+              _id: data._id
+            },
+            userId: userId,
+            path: `/transformations/${data._id}`
+          })
+          if (updatedImage) {
+            router.push(`/transformations/${updatedImage._id}`)
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    }
+    setIsSubmitting(false)
   }
   return (
     <Form {...form}>
@@ -119,7 +179,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
               </SelectTrigger>
               <SelectContent>
                 {Object.keys(aspectRatioOptions).map((key) => (
-                  <SelectItem key={key} value={key} className="select-item" >{key}</SelectItem>
+                  <SelectItem key={key} value={key} className="select-item" >{aspectRatioOptions[key as AspectRatioKey].label}</SelectItem>
                 ))}
               </SelectContent>
             </Select>
@@ -184,7 +244,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
             className="submit-button capitalize"
             disabled={isTransforming || newTransformation === null}
             onClick={onTransformHandler}
-            type="submit">
+            type="button">
             {isTransforming ? "Transforming..." : "Apply Transformation"}
           </Button>
           <Button
